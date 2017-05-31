cfe134dfe6ada37ee90c686e34315d817bdbd8e6
HYEYEON\HYEYEON <dusdl7070@gmail.com>
2017-05-31 12:05:10 +0900

    혜연 커밋

--- src/main/java/com/java/kosta/service/timeline/TimelineService.java
+++ src/main/java/com/java/kosta/service/timeline/TimelineService.java
@@ -3,6 +3,7 @@ package com.java.kosta.service.timeline;
 import java.util.List;
 
 import com.java.kosta.dto.note.PagingDTO;
+import com.java.kosta.dto.timeline.FilterDTO;
 import com.java.kosta.dto.timeline.TimelineDTO;
 import com.java.kosta.dto.user.UserVO;
 
@@ -16,5 +17,14 @@ public interface TimelineService {
 
 	// 테스트 용도
 	public int listAll();
+	
+	//필터 적용된 게시물을 페이징하기 위한 관심사에 따른 게시글 갯수 반환(혜연추가)
+	public int countFilterList(FilterDTO filter);
+	
+	//필터 적용에 따라서 게시글 반환(혜연추가)
+	public List<TimelineDTO> listFilter(FilterDTO filter,PagingDTO pageMaker);
+
+	public int maxValue();
+
 
 }
